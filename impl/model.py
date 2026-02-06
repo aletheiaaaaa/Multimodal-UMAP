0c9819fbdb5c6f0095d037ea98c9a71ec7e0bd23
aletheiaaaaa <carbon674@gmail.com>
2026-02-06 11:50:53 +0000

    update loss display

--- impl/model.py
+++ impl/model.py
@@ -474,7 +474,7 @@ class UMAPMixture:
             optimizer.zero_grad()
 
             if epoch % 50 == 0 or epoch == epochs - 1:
-                tqdm.write(f"Epoch {epoch}/{epochs}, Loss: {loss.item():.4f}")
+                tqdm.set_description(f"{desc} (loss: {loss.item():.4f})")
 
         return embeds
 
